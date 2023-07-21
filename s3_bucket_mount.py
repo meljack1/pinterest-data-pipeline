b346d7baebc3b17cc427e4395ba857bb2a5f004d
Mel Jack <melissa_jack@hotmail.co.uk>
2023-07-21 12:08:10 +0100

    Update most popular year calculation to show all categories and all years with at least 1 post

--- s3_bucket_mount.py
+++ s3_bucket_mount.py
@@ -144,10 +144,10 @@ df_most_popular_category_by_year = df_geo.select(col("ind"), year("timestamp").a
     .join(df_pin.select(col("ind"), col("category")), df_geo["ind"] == df_pin["ind"])\
     .drop("ind")\
     .groupBy("post_year", "category")\
-    .agg(count('category').alias("category_count"))
+    .agg(count('category').alias("category_count")).show()
 
-df_most_popular_category_by_year.select("post_year", "category", "category_count")\
-    .groupBy("post_year")\
-    .agg(max("category").alias("category"),\
-        max("category_count").alias("category_count"))\
-    .orderBy(col("post_year").asc()).show()
+## df_most_popular_category_by_year.select("post_year", "category", "category_count")\
+##    .groupBy("post_year")\
+##    .agg(max("category").alias("category"),\
+##        max("category_count").alias("category_count"))\
+##    .orderBy(col("post_year").asc()).show()
